78dfd71bcc2e79cbbffcf469ac14cfe1061b538e
chuandonghuang <1950439317@qq.com>
2023-12-18 18:17:26 +0800

    fix：临时提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+/*
+ * @Author: chuandonghuang 1950439317@qq.com
+ * @Date: 2023-12-12 09:39:30
+ * @LastEditors: chuandonghuang 1950439317@qq.com
+ * @LastEditTime: 2023-12-18 17:45:00
+ * @Description: 
+ */
 import {BrotherApp,ProVIderApp} from './view/Brother'
 import {SendRequest,ClearEffect} from './view/useEffct'
+import {BiliBiliApp} from './view/demo/Bilibili'
+import {HooksApp} from './view/Hooks'
+import {ReduxApp} from './view/redux'
+import {MeiTuan} from './view/demo/meituan/components/index'
 function App() {
   return(<>
-    <BrotherApp></BrotherApp>
-    <ProVIderApp></ProVIderApp>
-    <SendRequest></SendRequest>
-    <ClearEffect></ClearEffect>
+    {/* <BrotherApp></BrotherApp> */}
+    {/* <ProVIderApp></ProVIderApp> */}
+    {/* <SendRequest></SendRequest> */}
+    {/* <ClearEffect></ClearEffect> */}
+    {/* <HooksApp></HooksApp> */}
+    {/* <BiliBiliApp></BiliBiliApp> */}
+    {/* <ReduxApp></ReduxApp> */}
+    <MeiTuan></MeiTuan>
   </>)
   
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import store from './store/index.js'
 import { Provider } from 'react-redux'
 
