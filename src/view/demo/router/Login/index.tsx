a20fa0c43e5c315f5e2561b58f0fadbe22283294
chuandonghuang <1950439317@qq.com>
2023-12-19 20:42:32 +0800

    feat：声明式导航

---
+++ src/view/demo/router/Login/index.tsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom'
+export const LoginApp = ()=>{
+    return (<>
+        this is login page
+        {/* 声明式导航 */}
+        <Link to='/article'>跳转文章页</Link>
+    </>)
+}
